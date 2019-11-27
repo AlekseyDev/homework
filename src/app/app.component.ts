9e6760bb6ea8cd5af39bb043c8b6e00a87467851
Aleksey Krysov <akrysov@gmail.com>
2019-11-28 00:06:01 +0300

    выводим список уникальных значении поля continent, применили двойное связывание, в ts компонент передали значение и из компонента получили значение класса.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ICity} from '../dto/Interfaces';
 import { cities } from '../mock/cities';
 import {init} from 'protractor/built/launcher';
@@ -8,7 +8,32 @@ import {init} from 'protractor/built/launcher';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'homework';
   public cities: ICity[] = cities;
+  public selectedContinent: string;
+  public continents: Array<string>;
+  /* с выбором конкретного континента, подчеркнем его, добавив соответствующий стиль */
+  public classSelectedA = 'selectedAUnderline';
+
+  ngOnInit(): void {
+    if (cities !== null && cities !== undefined) {
+      // достанем уникальные наименования из поля cityInfo.continent
+      this.continents = [...new Set(cities.map(item => item.cityInfo.continent))];
+    }
+  }
+
+  public setSelected(continent: string) {
+    this.selectedContinent = continent;
+  }
+
+  public getSelectedAClass(continent: string): string {
+    if (this.selectedContinent !== null) {
+      if (continent === this.selectedContinent) {
+        return this.classSelectedA;
+      }
+    }
+    return '';
+  }
 }
+
