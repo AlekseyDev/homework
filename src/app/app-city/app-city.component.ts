bd88b06a2a0e8c99e36b781641c798031ac22fa2
Aleksey Krysov <akrysov@gmail.com>
2019-11-28 22:46:06 +0300

    выделил новый компонент элемента списка - города (карточка города).  в родительском остался цикл по отфильтрованным элементам, содержимое в отд. компоненте в карточке города

---
+++ src/app/app-city/app-city.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ICity} from '../../dto/Interfaces';
+
+@Component({
+  selector: 'app-city',
+  templateUrl: './app-city.component.html',
+  styleUrls: ['./app-city.component.css']
+})
+export class AppCityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input()
+  public cityItem: ICity;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MatCardModule, MatGridListModule} from '@angular/material';
 import { CitiesContinentFilterPipe } from '../pipes/cities-continent-filter.pipe';
+import { AppCityComponent } from './app-city/app-city.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CitiesContinentFilterPipe
+    CitiesContinentFilterPipe,
+    AppCityComponent
   ],
   imports: [
     BrowserModule,
